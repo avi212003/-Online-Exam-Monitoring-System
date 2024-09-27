79549d759be2534d4ee8118d429c71cb56ae2050
dileepreddychinneluka <dileepreddy2101@gmail.com>
2024-09-27 12:10:26 -0400

    Add login illustration image, update SignIn styles, and modify SignIn component

--- Online-Exam-Monitoring-System/src/pages/Signin.jsx
+++ Online-Exam-Monitoring-System/src/pages/Signin.jsx
@@ -1,26 +1,22 @@
 import React, { useState } from 'react';
-import '../styles/style.css'; 
-import login_illustration from '../assets/login_illustration.svg'
-// Make sure your CSS is imported
-//import React, {usestate} from 'react';
-//const [state,setState]=usestate(initialValue);
+import '../styles/Signin.css'; 
+import login_illustration from '../assets/login_illustration.svg';
 
 function SignIn() {
-  const [username,setUsername]=useState('');
-  const [password,setPassword]=useState('');
-  // State to manage the login message
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [msg, setMsg] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // You can handle form submission here. Example:
-    const formData = new FormData(event.target);
-    const username = formData.get('username');
-    const password = formData.get('password');
-
-    // Here you would send the form data to your backend using fetch or axios.
-    // Assuming a successful login, you could set a success/failure message in the state:
-    setMsg('Login successful!'); // Set the message here based on the response.
+    
+    // Here you can handle the login logic
+    if (username && password) {
+      setMsg('Login successful!'); // Set success message
+      // You would typically send a request to your backend here
+    } else {
+      setMsg('Please fill in all fields.');
+    }
   };
 
   return (
@@ -31,9 +27,11 @@ function SignIn() {
           <input
             className="input_field"
             type="text"
-            id="Username"
-            name="Username"
+            id="username"
+            name="username"
             placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
           <br />
@@ -41,8 +39,10 @@ function SignIn() {
             className="input_field"
             type="password"
             id="password"
-            name="psw"
+            name="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
           <br />
@@ -53,7 +53,7 @@ function SignIn() {
           <h5>{msg}</h5>
         </form>
         <div className="register_link">
-          New to A-EYE? <a href="/" className="black">Register</a>
+          Don't have an account? <a href="/" className="black">Register</a>
         </div>
       </div>
       <div className="row login_left">
