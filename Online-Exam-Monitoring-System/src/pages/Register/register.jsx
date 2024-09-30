3e8e6b29b0257853658527d2982932ebac8cf8a5
JainikD <jainikdesai30@gmail.com>
2024-09-30 16:52:02 -0400

    changed background, alignmet of card and added a font

--- Online-Exam-Monitoring-System/src/pages/Register/register.jsx
+++ Online-Exam-Monitoring-System/src/pages/Register/register.jsx
@@ -26,62 +26,66 @@ function Register() {
   return (
     <div className="container">
       <div className="register_right">
-        <div className="header">Register</div>
         <div className='content'>
-          <form onSubmit={handleSubmit} className="register_form">
-            {/* <div className="header">Register</div> */}
-            <input
-              className="input_field"
-              type="text"
-              id="Username"
-              name="username"
-              placeholder="Username"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            /><br />
-            <input
-              className="input_field"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            /><br />
-            <select
-              className="input_field"
-              id="gender_select"
-              name="gender"
-              value={formData.gender}
-              onChange={handleChange}
-            >
-              <option value="" disabled>Select Gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
-            </select><br />
-            <label className="label" htmlFor="image_file">Select Image</label><br />
-            <input
-              type="file"
-              id="image_file"
-              name="file"
-              style={{ display: 'none' }}
-              accept=".jpg, .png, .jpeg"
-              onChange={handleChange}
-              required
-            />
-            <input
-              className="submit_button"
-              type="submit"
-              value="Register"
-            /><br />
-            <div className="register_link">
-              Already have an account? <a href="/login" className="sign_in_instead">Sign In</a>
-            </div>
-          </form>
           <img className="register_image" src={register} alt="Register Illustration" />
+          <div className='card'>
+            <div className="header">Register</div>
+            <form onSubmit={handleSubmit} className="register_form">
+              {/* <div className="header">Register</div> */}
+              <input
+                className="input_field"
+                type="text"
+                id="Username"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              /><br />
+              <input
+                className="input_field"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              /><br />
+              <select
+                className="input_field"
+                id="gender_select"
+                name="gender"
+                value={formData.gender}
+                onChange={handleChange}
+              >
+                <option value="" disabled>Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select><br />
+              <label className="label" htmlFor="image_file">Select Image</label><br />
+              <input
+                type="file"
+                id="image_file"
+                name="file"
+                style={{ display: 'none' }}
+                accept=".jpg, .png, .jpeg"
+                onChange={handleChange}
+                required
+              />
+              <input
+                className="submit_button"
+                type="submit"
+                value="Register"
+              />
+                {/* <p className='register_text'>Register1</p> */}
+              <br />
+              <div className="register_link">
+                Already have an account? <a href="/login" className="sign_in_instead">Sign In</a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
