d978132c0a7028fce4ac3f4042ea5d68d70c9330
AbhishekSanthkumar <asanthakumar@albany.edu>
2024-09-30 23:10:45 -0400

    Update Signin.jsx
    SignIn page updated

--- Online-Exam-Monitoring-System/src/pages/Signin.jsx
+++ Online-Exam-Monitoring-System/src/pages/Signin.jsx
@@ -1,70 +1,70 @@
-import React, { useState } from 'react';
-import '../styles/Signin.css'; 
-import login_illustration from '../assets/login_illustration.svg';
+import { useState } from 'react'
+import './Signin.css'
+import register from '../assets/SignIn.png'
 
-function SignIn() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [msg, setMsg] = useState('');
+function Register() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type, files } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: type === 'file' ? files[0] : value
+    }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
-    // Here you can handle the login logic
-    if (username && password) {
-      setMsg('Login successful!'); // Set success message
-      // You would typically send a request to your backend here
-    } else {
-      setMsg('Please fill in all fields.');
-    }
+    console.log(formData);
   };
 
   return (
     <div className="container">
-      <div className="row">
-        <form onSubmit={handleSubmit} className="login_form">
-          <div className="header">Sign In</div>
-          <input
-            className="input_field"
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <br />
-          <input
-            className="input_field"
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <br />
-          <input className="submit_button" type="submit" value="Login" />
-          <br />
-          <br />
-          {/* Display the message */}
-          <h5>{msg}</h5>
-        </form>
-        <div className="register_link">
-          Don't have an account? <a href="/" className="black">Register</a>
+      <div className="register_right">
+        <div className='content'>
+          <img className="register_image" src={register} alt="Register Illustration" />
+          <div className='card'>
+            <div className="header">SignIn</div>
+            <form onSubmit={handleSubmit} className="register_form">
+              {/* <div className="header">Register</div> */}
+              <input
+                className="input_field"
+                type="text"
+                id="Username"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              /><br />
+              <input
+                className="input_field"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              /><br />
+              <input
+                className="submit_button"
+                type="submit"
+                value="Sign In"
+              />
+              <br />
+              <div className="register_link">
+                Don't have an account? <a href="/login" className="sign_in_instead">Register</a>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="row login_left">
-        <img
-          className="login_left_image"
-          src={login_illustration}
-          alt="Login Illustration"
-        />
       </div>
     </div>
   );
 }
 
-export default SignIn;
+export default Register;
